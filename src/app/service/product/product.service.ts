82f26265ba80dfdb6d3fa44805a49c33215c4a82
Kimas <gabrielpoyta@outlook.com>
2024-06-13 23:31:15 -0300

    Começando as rotas de product

--- src/app/service/product/product.service.ts
+++ src/app/service/product/product.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../interface/Product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
+  baseUrl = 'http://localhost:3000';
 
-  constructor() { }
+  getAllProducts(page: number): Observable<Product[]> {
+    const token = sessionStorage.getItem('token-api');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<Product[]>(`${this.baseUrl}/product?page=${page}`, {
+      headers,
+    });
+  }
 }
